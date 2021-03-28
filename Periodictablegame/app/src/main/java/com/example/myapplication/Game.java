cfeb7f3fc5340dd35420e8ba6910771b024f9590
Krissana-PPY <krissanapu@kkumail.com>
2021-03-28 18:01:25 +0700

    add icon and gameguesspicture

--- Periodictablegame/app/src/main/java/com/example/myapplication/Game.java
+++ Periodictablegame/app/src/main/java/com/example/myapplication/Game.java
@@ -1,13 +1,37 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Game extends AppCompatActivity {
+
+    Button guessPictureButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        guessPictureButton = findViewById(R.id.guessPictureButton);
+        backButton = findViewById(R.id.backButton);
+
+        guessPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  guessPicture = new Intent(getApplicationContext(), GuessPicture.class);
+                startActivity(guessPicture);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
